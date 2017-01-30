46cb1452ae3f5ca2ab445706498082ba804548ca
Davide Francesco Merico <hds619@gmail.com>
2017-01-30 17:10:02 +0100

    Add react dependency & start script

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import Express from 'express';
 let app = new Express ();
 let counter = 0;
 
-app.get ('*', (req, res) => {
+app.get ('/', (req, res) => {
   counter++;
 
-  res.writeHead ({ 'Content-Type': 'application/json' });
+  res.writeHead (200, { 'Content-Type': 'application/json' });
   res.end (JSON.stringify ({ counter: counter }));
 });
+
+app.get ('*', (req, res) => res.status(404).send ('Not found'));
 
 app.listen (process.env.PORT || 3000);
