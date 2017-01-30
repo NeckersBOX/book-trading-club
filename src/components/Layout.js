ab74dd86e723a35324a96bdb559b48abda54f5b0
Davide Francesco Merico <hds619@gmail.com>
2017-01-30 21:28:20 +0100

    Add menu bar & style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Layout = React.createClass ({
   render () {
     return (
       <div>
-        <h1>Layout</h1>
+        <div className="clearfix">
+          <ul className="unauth-menu">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </div>
+
         {this.props.children}
       </div>
     );
